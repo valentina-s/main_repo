52854b00185bba96152d26f96ad9f8fa442b417a
valentina-s <val_is_everywhere@yahoo.com>
2015-08-04 19:56:58 -0700

    Showing BookingID's - Plot will only work on full schedule

--- SpyreApp/app.py
+++ SpyreApp/app.py
@@ -1,5 +1,7 @@
 from spyre import server
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 import os
 
 class SimpleApp(server.App):
@@ -21,7 +23,6 @@ class SimpleApp(server.App):
 #										{"label":"Inputs with actions 2", "value":4},
 #		#        ]
 
-
     tabs = ["Table","Plot","Cost"]
 
     outputs = [{ "type" : "table",
@@ -29,11 +30,18 @@ class SimpleApp(server.App):
                     "control_id" : "button1",
                     "tab" : "Table",
                     "on_page_load":False},
+                {"type" : "plot",
+                "id":"show_plot",
+                "control_id":"button2",
+                "tab":"Plot",
+                "on_page_load":False},
                 {"type" : "table",
-                "id":"cost_table_id",
-                # "control_id":"button2",
-                "tab":"Cost",
-                "on_page_load":False}]
+                  "id":"cost_table_id",
+                  "control_id": "button3",
+                  "tab": "Cost",
+                  "on_page_load":False,
+                }
+                ]
                     #,{"type":"html","id":"html","control_id":"button1"}]
                     
 
@@ -43,13 +51,20 @@ class SimpleApp(server.App):
                      action_id = "table_id"),
                 dict(type="button",
                      id = "button2",
+                     label = "Show Plot",
+                     action_id = "table_id"),
+                dict(type="button",
+                     id = "button3",
                      label = "Calculate Cost",
-                     action_id = "cost_table_id")]
+                     action_id = "table_id")]
     
     
     def getData(self, params):
-        brokenBusID = params['brokenBusID']    
-    
+        brokenBusID = params['brokenBusID']   
+        # set a default filename so that I do not enter it every time
+        if params['filename']=='':
+            params['filename'] = 'fixed_width_file.csv'
+        print(params['filename'])
         filename = os.path.join(os.getcwd(),'data',params['filename'])
         data = pd.read_csv(filename,sep = "\t")
         self.bus_number = data['Run']
@@ -65,15 +80,31 @@ class SimpleApp(server.App):
 	
         #params['table1'] = True
 	# check if brokenBusID is in today's list schedule
-        if brokenBusID in data.Run.unique():
+        if brokenBusID not in data.Run.unique():
             print("This bus ID does not exist in today's schedule.")
             
 
         if  brokenBusID =='':
             return data
         else:
-            return data.loc[data['Run']==brokenBusID,]
-            # return data.loc[data['Run']==brokenBusID,'BookingId'].unique()
+            #return data.loc[data['Run']==brokenBusID,]
+            return pd.DataFrame(data.loc[data['Run']==brokenBusID,].BookingId.unique(),columns = ['BookingId'])
+            
+    def getPlot(self,params):
+        df = self.getData(params)
+        fig = plt.figure()
+        splt1 = fig.add_subplot(2,1,1)
+        splt1.plot(df.LAT)#,df.LON)
+        splt2 = fig.add_subplot(2,1,2)
+        splt2.plot(df['LON'])
+        return fig
+         
+    
+         
+         
+                  
+         
+         
 
 app = SimpleApp()
 
